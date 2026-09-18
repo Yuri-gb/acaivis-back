20ebbc06f7e45f374617a24ca7bdd9b3b7212731
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-18 14:38:38 -0300

    fix: corrige import dos testes de e-mail

--- src/test/java/com/acaivis/service/OrderEmailTemplateServiceTest.java
+++ src/test/java/com/acaivis/service/OrderEmailTemplateServiceTest.java
@@ -1,7 +1,7 @@
 package com.acaivis.service;
 
 import com.acaivis.model.Order;
-import com.acaivis.model.OrderEmailEvent;
+import com.acaivis.service.OrderEmailEvent;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
