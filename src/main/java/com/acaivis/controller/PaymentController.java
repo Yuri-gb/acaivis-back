964694ef459fb2faf1298c953c2a322871bcf116
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-18 22:56:33 -0300

    fix: explicitar construtor do PaymentController

--- src/main/java/com/acaivis/controller/PaymentController.java
+++ src/main/java/com/acaivis/controller/PaymentController.java
@@ -14,6 +14,7 @@ import com.acaivis.service.OrderService;
 import com.acaivis.service.CardCheckoutService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
