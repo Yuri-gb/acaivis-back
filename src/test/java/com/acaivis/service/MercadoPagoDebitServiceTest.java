e656c2b1963cb89c660b9c03facbfd71c8b41a82
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-19 06:03:43 -0300

    test: validar id de debito sem normalizacao

--- src/test/java/com/acaivis/service/MercadoPagoDebitServiceTest.java
+++ src/test/java/com/acaivis/service/MercadoPagoDebitServiceTest.java
@@ -62,7 +62,7 @@ class MercadoPagoDebitServiceTest {
         Map<?, ?> payment = (Map<?, ?>) ((List<?>) transactions.get("payments")).get(0);
         Map<?, ?> method = (Map<?, ?>) payment.get("payment_method");
 
-        assertEquals("debelo", method.get("id"));
+        assertEquals("elo", method.get("id"));
         assertEquals("debit_card", method.get("type"));
         assertEquals("token-1", method.get("token"));
         assertEquals(1, method.get("installments"));
