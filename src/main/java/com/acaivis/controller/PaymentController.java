90cb7136955d9f8da97a51658c9d922a5c77cbba
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-18 22:57:26 -0300

    fix: inicialização do PaymentController (#8)
    * fix: explicitar construtor do PaymentController
    
    * fix: marcar construtor principal do PaymentController

--- src/main/java/com/acaivis/controller/PaymentController.java
+++ src/main/java/com/acaivis/controller/PaymentController.java
@@ -14,6 +14,7 @@ import com.acaivis.service.OrderService;
 import com.acaivis.service.CardCheckoutService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,6 +40,7 @@ public class PaymentController {
     @Value("${mercadopago.webhook-secret:}")
     private String webhookSecret;
 
+    @Autowired
     public PaymentController(
             OrderRepository orders,
             MercadoPagoService mercadoPago,
